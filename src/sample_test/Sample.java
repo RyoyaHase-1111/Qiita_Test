5e9626d1b961e24cccdae872cb55a26adeb5f1ed
RyoyaHase-1111 <m2065013@st.t-kougei.ac.jp>
2020-10-10 11:03:57 +0900

    add

--- src/sample_test/Sample.java
+++ src/sample_test/Sample.java
@@ -1,6 +1,7 @@
 package sample_test;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Sample")
 public class Sample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +28,7 @@ public class Sample extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served att: ").append(request.getContextPath());
 	}
 
 	/**
